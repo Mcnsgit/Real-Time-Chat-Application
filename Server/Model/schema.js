336f7e129ee8abd67cb0fec5525363dae0fa22f7
mcnsgit <cardigamiguel221@gmail.com>
2023-06-24 22:18:14 +0100

    .diff

--- Server/Model/schema.js
+++ Server/Model/schema.js
@@ -1,29 +1,34 @@
-// {
-//  “_id” : ObjectId(“5809171b71e640556be904ef”),
-//  “name” : “Sudheesh Shetty”,
-//  “handle” : “sudheesh”,
-//  “password” : “556624370”,
-//  “phone” : “8888888888”,
-//  “email” : “sudheeshshetty @gmail.com”,
-//  “friends” : [
-//         {
-//       “name” : “abc”,
-//       “status” : “Friend”
-//     },
-// {
-//       “name” : “xyz”,
-//       “status” : “Friend”
-// }
-//  ],
-//  “__v” : 0
-// }
+var mongoose = require('mongoose');
 
-/* Here, the status of each member can be:
+var Schema = mongoose.Schema;
 
-    Friend: Stating that the member is a friend.
-    Pending: If the member has not yet accepted.
-    Blocked: If the member has blocked the other member.
+mongoose.connect("mongodb://localhost:3001/chat");
 
-Suppose the member has rejected a chat request. The sender can then send a chat request again. A user can also save the messages by creating an extra collection. Each document will have the message, sender, receiver, and time.
+mongoose.connection.on('open', function (ref) {
+    console.log('Connected to mongo server.');
+});
+mongoose.connection.on('error', function (err) {
+    console.log('Could not connect to mongo server!');
+    console.log(err);
+});
 
-So design your database according to your specific needs and how you want to handle messages.*/
+mongoose.connect('mongodb://localhost/mongodb');
+
+module.exports.user = mongoose.model('User', new Schema({
+    name: String,
+    handle: String,
+    password: String,
+    phone: String,
+    email: String,
+    friends: []
+}, { strict: false }));
+module.exports.online = mongoose.model('online', new Schema({
+    handle: String,
+    connection_id: String
+}));
+module.exports.messages = mongoose.model('message', new Schema({
+    message: String,
+    sender: String,
+    reciever: String,
+    date: Date
+}));
